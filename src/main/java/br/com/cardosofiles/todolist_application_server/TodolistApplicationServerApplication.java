b5675839b1344661de1a113e8e60a00df4571ff6
cardosofiles <cardosofiles@outlook.com>
2025-05-26 22:17:05 -0300

    feat: adding the Lombok library and configuring spring boot

---
+++ src/main/java/br/com/cardosofiles/todolist_application_server/TodolistApplicationServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.cardosofiles.todolist_application_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistApplicationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistApplicationServerApplication.class, args);
+	}
+
+}
